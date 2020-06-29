385c87f5378e4c71bfb335370fd760e42dbf2644
xcatliu <xcatliu@gmail.com>
2020-06-29 10:38:55 +0800

    Fix copy

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -19,7 +19,9 @@ export async function copyPagicFile(pathToPagicRoot: string, dest: string) {
     await ensureDirAndCopy(src, dest, { overwrite: true });
   } else {
     const res = await fetch(import.meta.url.replace(/\/src\/utils\/copy\.ts$/, `/${pathToPagicRoot}`));
-    const content = await res.text();
-    await ensureDirAndWriteFileStr(dest, content);
+    await fs.ensureDir(path.dirname(dest));
+    // https://stackoverflow.com/q/61945050/2777142
+    const file = await Deno.open(dest, { create: true, write: true });
+    await Deno.writeAll(file, new Uint8Array(await res.arrayBuffer()));
   }
 }
