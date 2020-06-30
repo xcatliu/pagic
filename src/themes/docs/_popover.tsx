97ebe3ba35a42236dd4fdb6923d919c61ea26b36
xcatliu <xcatliu@gmail.com>
2020-06-30 23:43:20 +0800

    Fix nextTick bug

--- src/themes/docs/_popover.tsx
+++ src/themes/docs/_popover.tsx
@@ -3,7 +3,7 @@ import React from 'https://dev.jspm.io/react@16.13.1';
 // @deno-types="https://deno.land/x/types/react-dom/v16.13.1/react-dom.d.ts"
 import ReactDOM from 'https://dev.jspm.io/react-dom@16.13.1';
 
-import { classnames, nextTick } from './_utils.tsx';
+import { classnames } from './_utils.tsx';
 
 interface PopoverProps {
   content: React.ReactNode;
