fd708362d791faed07d5c337c9b2571349634c02
xcatliu <xcatliu@users.noreply.github.com>
2020-07-02 13:03:12 +0000

    deploy: 3848027c0800afec0e389d3a7d07e9b3bd8c4964

--- hello_props.js
+++ hello_props.js
@@ -10,6 +10,6 @@ export default {
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js" }),
-        React.createElement("script", { src: "/main.js", type: "module" })),
+        React.createElement("script", { src: "/index.js", type: "module" })),
     'author': "xcatliu"
 };
--- index_props.js
+++ index_props.js
@@ -11,7 +11,7 @@ export default {
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js" }),
-        React.createElement("script", { src: "/main.js", type: "module" })),
+        React.createElement("script", { src: "/index.js", type: "module" })),
     'author': "xcatliu",
     'toc': React.createElement("aside", { dangerouslySetInnerHTML: {
             __html: '<nav class="toc"><ol></ol></nav>'
