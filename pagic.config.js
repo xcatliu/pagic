3b70adff8a487f35bb2a478b2b90602eb8c2d8fd
xcatliu <xcatliu@users.noreply.github.com>
2020-07-17 15:12:56 +0000

    deploy: 6feaf5fb69f0e28f04f6fa15a33d2d9426d4e7de

--- pagic.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    srcDir: '.',
-    base: '/pagic/',
-    title: 'Pagic',
-    description: 'The easiest way to generate static html page from markdown, built with Deno! 🦕',
-    github: 'https://github.com/xcatliu/pagic',
-    ignore: ['LICENSE', 'mod.ts', 'test', 'src']
-};
