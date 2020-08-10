3951b5725799efac67c5f90b0a309675dc142390
xcatliu <xcatliu@gmail.com>
2020-08-10 12:09:43 +0800

    feat: upgrade deno std to 0.64.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
-export * as fs from 'https://deno.land/std@0.61.0/fs/mod.ts';
-export * as path from 'https://deno.land/std@0.61.0/path/mod.ts';
-export * as colors from 'https://deno.land/std@0.61.0/fmt/colors.ts';
+export * as fs from 'https://deno.land/std@0.64.0/fs/mod.ts';
+export * as path from 'https://deno.land/std@0.64.0/path/mod.ts';
+export * as colors from 'https://deno.land/std@0.64.0/fmt/colors.ts';
