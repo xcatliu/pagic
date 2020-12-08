b1a0bb3f72a6d718ae8bafebfa2512e800bb121b
xcatliu <xcatliu@gmail.com>
2020-12-08 15:15:16 +0800

    Update pagic.cn

--- pagic.cn/pagic.config.tsx
+++ pagic.cn/pagic.config.tsx
@@ -17,42 +17,42 @@ export default {
   nav: [
     {
       text: '文档',
-      link: '/docs/introduction.html'
+      link: '/docs/introduction.html',
     },
     {
       text: '主题',
-      link: '/themes/'
+      link: '/themes/',
     },
     {
       text: '插件',
-      link: '/plugins/'
+      link: '/plugins/',
     },
     {
       text: '博客',
-      link: '/blog/'
+      link: '/blog/',
     },
     {
       text: '关于',
-      link: '/about/'
-    }
+      link: '/about/',
+    },
   ],
   sidebar: {
-    '/zh-CN/docs/': [
-      'zh-CN/docs/introduction.md',
-      'zh-CN/docs/usage.md',
-      'zh-CN/docs/config.md',
-      'zh-CN/docs/content.md',
-      'zh-CN/docs/layout.md',
-      'zh-CN/docs/themes.md',
-      'zh-CN/docs/plugins.md',
-      'zh-CN/docs/blog.md',
-      'zh-CN/docs/i18n.md',
-      'zh-CN/docs/deployment.md',
-      'zh-CN/docs/demos.md',
-      'zh-CN/docs/limitations.md'
-    ]
+    '/docs/': [
+      'docs/introduction.md',
+      'docs/usage.md',
+      'docs/config.md',
+      'docs/content.md',
+      'docs/layout.md',
+      'docs/themes.md',
+      'docs/plugins.md',
+      'docs/blog.md',
+      'docs/i18n.md',
+      'docs/deployment.md',
+      'docs/demos.md',
+      'docs/limitations.md',
+    ],
   },
   blog: {
-    root: '/blog/'
-  }
+    root: '/blog/',
+  },
 };
