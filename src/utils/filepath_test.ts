0905262826579413095555c0c4026c156290a811
xcatliu <xcatliu@gmail.com>
2021-04-12 14:32:33 +0800

    Revert "Fix localeCompare in diff env"
    This reverts commit 0e9392fc83d974124863c5ec83a793a73ea44b5f.

--- src/utils/filepath_test.ts
+++ src/utils/filepath_test.ts
@@ -71,23 +71,14 @@ Deno.test('[findNearestLayoutPath]', () => {
   asserts.assertEquals(findNearestLayoutPath('foo/bar/baz.md', ['foo/_layout.tsx', '_layout.tsx']), 'foo/_layout.tsx');
   asserts.assertEquals(findNearestLayoutPath('bar/baz.md', ['bar/_layout.tsx', '_layout.tsx']), 'bar/_layout.tsx');
 });
-const sort = (arr: string[]) =>
-  arr.sort((a: string, b: string) => {
-    const compareResult = a.localeCompare(b);
-    if (compareResult > 0) {
-      return 1;
-    } else if (compareResult === 0) {
-      return 0;
-    } else {
-      return -1;
-    }
-  });
+const sort = (arr: string[]) => arr.sort((a: string, b: string) => a.localeCompare(b));
 Deno.test('[walk]', async () => {
+  console.log('.'.localeCompare('_'));
   asserts.assertEquals(sort(await walk('test/fixtures/walk')), [
-    '_header.tsx',
-    '_layout.tsx',
     '.bar',
     '.foo/foo.md',
+    '_header.tsx',
+    '_layout.tsx',
     'a/_bar.tsx',
     'a/_layout.tsx',
     'a/bar',
