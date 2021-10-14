df7ab83a4a049012d988cb17ced3f9f94b8de2db
xcatliu <xcatliu@gmail.com>
2021-10-14 16:24:31 +0800

    fix: Wait for 1 second to close server

--- src/utils/serve_test.ts
+++ src/utils/serve_test.ts
@@ -13,6 +13,10 @@ Deno.test('[serve]', async () => {
   asserts.assertEquals(await not_found.status, 404);
   asserts.assertEquals(await not_found.text(), 'Not Found');
   server.close();
+  // Wait for 1 second to close server
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
 });
 
 Deno.test('[serve] root option', async () => {
@@ -24,4 +28,8 @@ Deno.test('[serve] root option', async () => {
   const no_toc = await fetch('http://127.0.0.1:8000/foo/no_toc.md');
   asserts.assertEquals(await no_toc.text(), 'foo\n');
   server.close();
+  // Wait for 1 second to close server
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
 });
