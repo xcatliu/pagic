0a67264896878f36ac9466702849186c8598c242
TianyuGao <tianyu8125@163.com>
2022-08-12 22:11:12 +0800

    fix: update import

--- deps.ts
+++ deps.ts
@@ -18,20 +18,20 @@ import * as ReactDOMServer from 'https://esm.sh/react-dom@17.0.2/server';
 export { React, ReactDOM, ReactDOMServer };
 
 // @deno-types="./src/types/any.d.ts"
-import frontMatter from 'https://dev.jspm.io/front-matter@4.0.2';
+import frontMatter from 'https://jspm.dev/front-matter@4.0.2';
 // @deno-types="./src/types/any.d.ts"
-import htmlToTextMod from 'https://dev.jspm.io/html-to-text@6.0.0';
+import htmlToTextMod from 'https://jspm.dev/html-to-text@6.0.0';
 const { htmlToText } = htmlToTextMod;
 // @deno-types="./src/types/any.d.ts"
-import MarkdownIt from 'https://dev.jspm.io/markdown-it@11.0.1';
+import MarkdownIt from 'https://jspm.dev/markdown-it@11.0.1';
 // @deno-types="./src/types/any.d.ts"
-import reactElementToJSXStringMod from 'https://dev.jspm.io/react-element-to-jsx-string@14.3.1';
+import reactElementToJSXStringMod from 'https://jspm.dev/react-element-to-jsx-string@14.3.1';
 const reactElementToJSXString = reactElementToJSXStringMod.default;
 // @deno-types="./src/types/any.d.ts"
-import reactHtmlParserMod from 'https://dev.jspm.io/react-html-parser@2.0.2';
+import reactHtmlParserMod from 'https://jspm.dev/react-html-parser@2.0.2';
 const reactHtmlParser = reactHtmlParserMod.default;
 // @deno-types="./src/types/any.d.ts"
-import * as typescriptMod from 'https://dev.jspm.io/typescript@4.0.3';
+import * as typescriptMod from 'https://jspm.dev/typescript@4.0.3';
 const typescript = typescriptMod.default;
 
 export { frontMatter, htmlToText, MarkdownIt, reactElementToJSXString, reactHtmlParser, typescript };
